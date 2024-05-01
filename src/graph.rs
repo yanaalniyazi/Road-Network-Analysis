d88bbf8a55c9e242547760fedcf1dbb361f621bb
yanaalniyazi <yanaalniyazi@gmail.com>
2024-04-30 20:18:59 -0400

    Updated graph.rs

--- src/graph.rs
+++ src/graph.rs
@@ -6,3 +6,30 @@ use std::path::Path;
 use std::collections::VecDeque;
 use rand::seq::SliceRandom;
 
+
+pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+
+pub fn read_graph_from_file(file_path: &str) -> Graph<i32, ()> {
+    let mut graph: HashMap<i32, Vec<i32>> = HashMap::new();
+    println!("Pennsylvania Network Data");
+
+    if let Ok(lines) = read_lines(file_path) {
+        for line in lines {
+            if let Ok(ip) = line {
+                if ip.starts_with('#') {
+                    continue;
+                }
+                let vec: Vec<i32> = ip.split_whitespace().map(|s| s.parse().unwrap()).collect();
+                let from = vec[0];
+                let to = vec[1];
+                graph.entry(from).or_insert_with(Vec::new).push(to);
+            }
+        }
+    }
